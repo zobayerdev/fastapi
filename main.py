56f1fb34b995eda24ffe2716984f1178c71a4976
zobayersv <nayem.bdcit@gmail.com>
2026-08-14 15:48:07 +0600

    GET API structure Done

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    age: int
+    name: str
+    email: str
+
+#* create home route with GET method
+@app.get("/")
+async def home():
+    return {
+        "Message":"Welcome to the FastAPI application!"
+    }
+
+#* create the userlist
+@app.get("/users")
+async def userList():
+    return{
+        "users": [
+            {"id": 1, "name": "John Doe"},
+            {"id": 2, "name": "Jane Smith"},
+            {"id": 3, "name": "Alice Johnson"}
+        ]
+    }
+
+#* create the contact route with query parameter (default value of name is 'null')
+@app.get("/contact")
+async def get_contact(name: str = 'null'):
+    return {
+        "Name":name
+    }
+
+#* create the products route with query parameter (default value of limit is 10)
+@app.get("/products")
+async def get_products(limit: int = 10):
+    return {
+        "limit": limit
+    }
+
+# * default parameter & optional parameter
+@app.get("/items")
+async def get_items(name: str = None, price: int = 10):
+    return {
+        "name": name,
+        "price": price
+    }
+
+
+# * POST API Method
+@app.post("/create_user")
+async def create_user(user: User):
+    return{
+        "Message":"User created successfully!",
+        "status": 200,
+        "user":user
+    }
+    
